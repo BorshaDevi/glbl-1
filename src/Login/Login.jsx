dd5136dc1f3f7f0dbbe2ddaeb50df6cbe4e49c4e
unknown <borshadevi27@gmail.com>
2025-03-29 10:40:35 +0600

    add login

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,15 +1,28 @@
+
+
 const Login=()=>{
-    const handleForm=(e)=>{
+    
+    const handleForm=async(e)=>{
         e.preventDefault()
+        const form=e.target
+        const email=form.email.value
+        const password =form.password.value
+        const login={
+            email,
+            password
+        }
+       await axi
 
     }
     return (
         <div className="flex justify-center items-center h-screen">
-            <form className="bg-white p-8 shadow-md">
+            <form onSubmit={handleForm} className="bg-white p-8 shadow-md">
                 <h1 className="font-bold text-xl  text-center mb-5"> Login</h1>
-            <input type="email" onChange= placeholder="Enter your email"  className="border border-black p-2" ></input>
+            <input type="email"  name="email" placeholder="Enter your email"  className="border border-black p-2" ></input>
             <br></br>
-            <input type="email" placeholder="******" className="border border-black mt-5 p-2" ></input>
+            <input type="password" name="password" placeholder="******" className="border border-black mt-5 p-2" ></input>
+            <br />
+            <input type="submit"  value='Submit' className="border border-y-4  border-emerald-900 mt-5 p-2 bg-emerald-400 font-bold" ></input>
             </form>
            
         </div>
