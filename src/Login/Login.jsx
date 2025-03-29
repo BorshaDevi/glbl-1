6f6e04ada1975a3311adae89fcfe5aaed88e6367
unknown <borshadevi27@gmail.com>
2025-03-29 09:34:26 +0600

    added text

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,9 +1,13 @@
 const Login=()=>{
+    const handleForm=(e)=>{
+        e.preventDefault()
+
+    }
     return (
         <div className="flex justify-center items-center h-screen">
             <form className="bg-white p-8 shadow-md">
                 <h1 className="font-bold text-xl  text-center mb-5"> Login</h1>
-            <input type="email" placeholder="Enter your email"  className="border border-black p-2" ></input>
+            <input type="email" onChange= placeholder="Enter your email"  className="border border-black p-2" ></input>
             <br></br>
             <input type="email" placeholder="******" className="border border-black mt-5 p-2" ></input>
             </form>
